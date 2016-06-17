7365a98be456c3d5ed4943df2e4f7017cb5df76a
ferna <ferna@DESKTOP-NLCCDC7>
2016-06-17 10:56:39 -0300

    Add a gig created

---
+++ Mosh/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using Mosh.Models;
+using Mosh.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Mosh.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public GigsController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = db.Genres.ToList(),
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Mosh/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Mosh.Models;
+using System.Collections.Generic;
+
+namespace Mosh.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
